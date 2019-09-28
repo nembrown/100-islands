f51d2b2e74621dec3cf22922f415b2e91707c47b
Norah Brown <norahbrown@uvic.ca>
2019-09-28 10:21:17 -0700

    Updated with full d34S data

--- Food web idea/R files/Current scripts/Soil 34s.R
+++ Food web idea/R files/Current scripts/Soil 34s.R
@@ -1,6 +1,22 @@
 setwd("C:/Users/Norah/Dropbox/Projects/100-islands/Food web idea/Data by person")
 soil_s<-read.csv("C:Norah.data/soil_s.csv")
 
+
+# load packages ----------------------------------------------------------
+library(rlang)
+library(tidyr)
+library(vegan)
+library(ggplot2)
+library(ggcorrplot)
+library(doBy)
+library(plyr)
+library(dplyr)
+library(doBy)
+library(cowplot)
+library(viridis)
+library(matrixStats)
+library(tidyverse)
+
 head(soil_s)
 
 subsample_owen_soils<-read.csv("C:Owen's data/subsample_owen_soil.csv")
@@ -10,9 +26,9 @@ head(subsample_owen_soils)
 subsample_owen_soils_s<-merge(subsample_owen_soils, soil_s, by="unq_plot", all=TRUE)
 head(subsample_owen_soils_s)
 
-ggplot(subsample_owen_soils_s,aes(y=d15n, x=d34s, colour=node, size=4))+geom_point()
-ggplot(subsample_owen_soils_s,aes(y=d34s, x=shore_dist, colour=node, size=4))+geom_point()
+ggplot(subsample_owen_soils_s,aes(x=s, y=d34s, colour=node))+geom_point()
+ggplot(subsample_owen_soils_s,aes(y=d34s, x=lat, colour=node, size=4))+geom_point()
 
 
 
-ggplot(subsample_owen_soils_s,aes(y=d15n, x=n, colour=node, size=2))+geom_point()
+ggplot(subsample_owen_soils_s,aes(y=d15n, x=n, colour=node, size=1))+geom_point()
