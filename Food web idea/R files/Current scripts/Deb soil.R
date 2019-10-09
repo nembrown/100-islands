ff85a7b9bd0202b177d68ada0c4ca8a30c53f9ab
Norah Brown <norahbrown@uvic.ca>
2019-10-09 08:32:46 -0700

    Merging lab numbers and sample ids

---
+++ Food web idea/R files/Current scripts/Deb soil.R
@@ -0,0 +1,17 @@
+library(here)
+
+#here() starts at C:/Users/norahbrown/Dropbox/Projects/100-islands
+
+
+deb_soil_matching<-read.csv("C:Food web idea//Data by person//Norah.data//deb_soil_matching.csv")
+subsample_deb_soil<-read.csv("C:Food web idea//Data by person//Deb.data//subsample_deb_soil.csv")
+
+head(deb_soil_matching)
+
+head(subsample_deb_soil)
+
+deb_soil_merged<-merge(deb_soil_matching[,c(1,7,11)], subsample_deb_soil)
+head(deb_soil_merged)
+
+write.csv(deb_soil_merged, "C:Food web idea//Data by person//Norah.data//deb_soil_merged.csv")
+
