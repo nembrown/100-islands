7ef40b08f77f0aca72e67afc45ec06c6dd071070
Norah Brown <norahbrown@uvic.ca>
2019-11-20 13:39:50 -0800

    updated script for smoothness in running, changed from 1km to 2km radius to be consistent with Sara's data

--- Biodiversity idea/R files/Current scripts/AssignClosestPointsToPoints_radius.R
+++ Biodiversity idea/R files/Current scripts/AssignClosestPointsToPoints_radius.R
@@ -12,20 +12,19 @@ library(ggmap) # ggplot functionality for maps ---> dplyr, purr is dependency
 library(ggsn) # for scale bars/north arrows in ggplots
 library(maps)
 library(mapdata)
-
-setwd("C:/Users/Norah/Dropbox/Projects/100-islands/Biodiversity idea")
+library(here)
 
 #get Ben's data in right format
-ben_habitat_data<-read.csv("C:Ben.data//beachseine_calvert_NB//hakaiBS_habitat_20142018.csv")
+ben_habitat_data<-read.csv("C:Biodiversity idea//Ben.data//beachseine_calvert_NB//hakaiBS_habitat_20142018.csv")
 head(ben_habitat_data)
-ben_habitat_data_simple<-ben_habitat_data[,c(1:3)]
+ben_habitat_data_simple<-ben_habitat_data[,c(3:5)]
 
 ben_habitat_data_simple.SP_new <- st_as_sf(ben_habitat_data_simple, coords = c("long", "lat"), crs = 4326)%>% st_transform(3035)
 head(ben_habitat_data_simple.SP_new)
 
 #Transect-level information
-setwd("C:/Users/Norah/Dropbox/Projects/100-islands/Food web idea")
-by_tran_master<-read.csv("C:Data by person//Norah.data//by_tran_master.csv")
+
+by_tran_master<-read.csv("C:Food web idea//Data by person//Norah.data//by_tran_master.csv")
 by_tran_master<-by_tran_master[,-1]
 data_subset2 <- by_tran_master[ , c("easting", "northing")]
 by_tran_master_no_na<- by_tran_master[complete.cases(data_subset2), ]
@@ -36,15 +35,14 @@ df.SF_transects_simple_new<- df.SF_transects_simple %>% st_transform(3035)
 
 #https://gis.stackexchange.com/questions/229453/create-a-circle-of-defined-radius-around-a-point-and-then-find-the-overlapping-a
 
-# Buffer circles by 1000m -- creates polygons around the 
-dat_circles <- st_buffer(df.SF_transects_simple_new, dist = 1000)
+# Buffer circles by 2000m -- creates polygons around the 
+dat_circles <- st_buffer(df.SF_transects_simple_new, dist = 2000)
 
-#which of the beachseines fall within 1km radius of the transect
+#which of the beachseines fall within 2km radius of the transect
 transects_beach_joined = st_join(ben_habitat_data_simple.SP_new, dat_circles, left=FALSE)
 
 head(transects_beach_joined)
 
 transects_beach_joined <-transects_beach_joined  %>% st_set_geometry(NULL)
 transects_beach_joined<-as.data.frame(transects_beach_joined)
-setwd("C:/Users/Norah/Dropbox/Projects/100-islands/Biodiversity idea")
-write.csv(transects_beach_joined, "C:Output files//paired_sites_by_radius.csv")
+write.csv(transects_beach_joined, "C:Biodiversity idea//Output files//paired_sites_by_radius.csv")
