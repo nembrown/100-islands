17bc788272d272ab4baddee0f67c9439f9946c93
Norah Brown <norahbrown@uvic.ca>
2020-02-12 15:59:40 -0800

    adding canoe skid as a human indictor

--- Food web idea/R files/Current scripts/arch sites cleaning.R
+++ Food web idea/R files/Current scripts/arch sites cleaning.R
@@ -6,9 +6,10 @@ arch_sites$midden_feature <- ifelse(grepl("Shell Midden", arch_sites$TY_TYPOLOGY
 arch_sites$CMT <- ifelse(grepl("Culturally Modified Tree", arch_sites$TY_TYPOLOGY), "yes", "no")
 arch_sites$clam_garden <- ifelse(grepl("Clam Garden", arch_sites$TY_TYPOLOGY), "yes", "no")
 arch_sites$fish_feature <- ifelse(grepl("Fish Trap", arch_sites$TY_TYPOLOGY), "yes", "no")
+arch_sites$canoe_skid <- ifelse(grepl("Canoe Skid", arch_sites$TY_TYPOLOGY), "yes", "no")
 
 arch_sites_selected<- arch_sites %>% dplyr::select(BORDENNUMBER, MR_GISUTMEASTING, MR_GISUTMNORTHING, 
-                                            midden_feature, fish_feature, CMT, clam_garden) %>% 
+                                            midden_feature, fish_feature, CMT, clam_garden, canoe_skid) %>% 
                                             rename(site_id=BORDENNUMBER , easting=MR_GISUTMEASTING , northing=MR_GISUTMNORTHING)
 head(arch_sites_selected)
 
