315e1ed7f3b50b28cc3909f5ed7b1fc217ddc16e
Norah Brown <norahbrown@uvic.ca>
2020-02-12 15:59:17 -0800

    Cleaning up the script - adding in new measurement models for some of the latent variables  - problem is too many shared variables

--- Food web idea/R files/Current scripts/SEM_lavaan.R
+++ Food web idea/R files/Current scripts/SEM_lavaan.R
@@ -1,27 +1,22 @@
-install.packages("lavaan", dependencies = TRUE)
-install.packages("ggm", dependencies = TRUE)
+# install.packages("lavaan", dependencies = TRUE)
+# install.packages("ggm", dependencies = TRUE)
 
 library(lavaan)
+master_transect<-read.csv("C:Biodiversity idea//Output files//master_transect.csv")
 
-head(fish_richness_merged_tran_arch)
 
-#pair down the 
-fish_richness_merged_isl_colnames<-c( "fish_biomass_bym3_mean", "fish_bycatch_biomass", "MEAN_kparea2k", "MEAN_egarea2k",
-                                     "SLOPE", "PA_norml", "WAVE_EXPOSURE", "Rock", "slope", "site_sum_by_isl", "ravens", "eagles") 
-                                     
-View(fish_richness_merged_tran_arch_2)  
-#site_sum_by_isl, MEAN_kparea, MEAN_egarea need to be logged
 
-fish_richness_merged_tran_arch_2$log_site_sum_by_isl <- log(fish_richness_merged_tran_arch_2$site_sum_by_isl+1)
-fish_richness_merged_tran_arch_2$log_MEAN_kparea2k <- log(fish_richness_merged_tran_arch_2$MEAN_kparea2k+1)
-fish_richness_merged_tran_arch_2$log_MEAN_egarea2k <- log(fish_richness_merged_tran_arch_2$MEAN_egarea2k+1)
-fish_richness_merged_tran_arch_2$log_Rock<- log(fish_richness_merged_tran_arch_2$Rock+1)
-fish_richness_merged_tran_arch_2$midden_feature_sem[is.na(fish_richness_merged_tran_arch_2$midden_feature_sem)] <- 0
+# #pair down the 
+# fish_richness_merged_isl_colnames<-c( "fish_biomass_bym3_mean", "fish_bycatch_biomass", "MEAN_kparea2k", "MEAN_egarea2k",
+#                                      "SLOPE", "PA_norml", "WAVE_EXPOSURE", "Rock", "slope", "site_sum_by_isl", "ravens", "eagles") 
+#                                      
+
+
 
 
 
 N15_model<-'#latent variables as responses
-            fish_biomass_bym3_mean + fish_bycatch_biomass + log_MEAN_kparea2k + log_MEAN_egarea2k + SLOPE + PA_norml + WAVE_EXPOSURE + log_Rock + slope ~ vector_pres
+            fish_biomass_bym3_mean + fish_bycatch_biomass + log_MEAN_kparea2k + log_MEAN_egarea2k + SLOPE + PA_norml + WAVE_EXPOSURE + log_Rock + slope  ~ vector_pres
             
             fish_biomass_bym3_mean + fish_bycatch_biomass + log_MEAN_kparea2k + log_MEAN_egarea2k + SLOPE + PA_norml + WAVE_EXPOSURE + log_Rock + slope ~ human_pres
             
@@ -50,22 +45,20 @@ N15_model<-'#latent variables as responses
 
             
             #latent variables measurement models
-            vector_pres =~ ravens + eagles
-            human_pres =~ midden_feature_sem
-            algae_biomass_shore =~ log_site_sum_by_isl
-            human_pres =~ d15n
-            algae_biomass_shore = ~d15n
+            vector_pres =~ ravens + eagles + midi
+            human_pres =~ midden_feature_sem + cult_imp_plant_richness + fish_feature_sem + d15n
+            algae_biomass_shore =~ log_site_sum_by_isl + d15n
             marine_animal_biomass_shore =~ d15n '
 
 
-fit <- sem(N15_model, data=fish_richness_merged_tran_arch_2,  missing = "ML")
+fit <- sem(N15_model, data=master_transect,  missing = "ML")
 summary(fit, fit.measures=TRUE)
 
+varTable(fit)
 
 
 
-
-length(na.omit(fish_richness_merged_tran_arch_2$d15n))                    
+length(na.omit(master_transect$d15n))                    
 
 ?varTable()
 
@@ -107,7 +100,7 @@ N15_model_novector<-'#latent variables as responses
             algae_biomass_shore = ~d15n
             marine_animal_biomass_shore =~ d15n '
 
-fit2 <- sem(N15_model_novector, data=fish_richness_merged_tran_arch_2)
+fit2 <- sem(N15_model_novector, data=master_transect)
 summary(fit2, fit.measures=TRUE)
 
 varTable(fit2)
@@ -164,6 +157,6 @@ N15_model_novector2<-'#latent variables as responses
             true_fish_biomass =~ fish_biomass_bym3_mean
             true_invert_biomass =~ fish_bycatch_biomass '
 
-fit3 <- sem(N15_model_novector2, data=fish_richness_merged_tran_arch_2)
+fit3 <- sem(N15_model_novector2, data=master_transect)
 summary(fit3, fit.measures=TRUE)
 varTable(fit3)
