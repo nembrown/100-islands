e37385fe6b2c483c6fc9e11402da81e031c474e6
Norah Brown <norahbrown@uvic.ca>
2020-02-12 16:00:30 -0800

    new script to calculate the richness of culturally important plant species ... i.e. how many CIPS per transect??

--- Food web idea/R files/Current scripts/culturally_imp_plants.R
+++ Food web idea/R files/Current scripts/culturally_imp_plants.R
@@ -1,3 +1,29 @@
+library(rlang)
+library(tidyr)
+library(vegan)
+library(ggplot2)
+library(ggcorrplot)
+library(doBy)
+library(plyr)
+library(dplyr)
+library(doBy)
+library(cowplot)
+library(viridis)
+library(matrixStats)
+library(tidyverse)
+
+#this loads data from "Habitation data" R script
+
+longform_plant_percentcover<-read.csv("C:Food web idea//Data by person//Kalina.data/Deb_Owen_veg_combined_complete_filled.csv", header=TRUE, sep=",")
+longform_plant_percentcover<-longform_plant_percentcover[,-c(1)]
+head(longform_plant_percentcover)
+
+longform_plant_percentcover_owen <- longform_plant_percentcover %>% filter(person=="Owen")
+head(longform_plant_percentcover_owen)
+longform_plant_percentcover_owen$cover<-as.numeric(longform_plant_percentcover_owen$cover)
+
+
+
 ## CULTURALLY IMPORTANT SPECIES LIST
 # Create column - cult important (yes/no), then merge
 cult.imp.sp <- c("rhgr",
@@ -36,21 +62,24 @@ cult.imp.sp <- c("rhgr",
                  "tabr",
                  "pico")
 # Creating new dataframe, filter by cult sp
-plant_data_cult <- plant_data %>% filter(species %in% cult.imp.sp)
-view(plant_data_cult)
-# Plot bar chart for cult sp (how many instances of a CIP per node) *not the best one
-ggplot(data = plant_data_cult %>% filter(pres_abs == "1"), aes(x = node, fill = node)) +
-  geom_bar()
-# Create wide dataframe (node by CIP) and fill with instances of presence
-plant_data_cult_wide <- plant_data_cult %>% group_by(node, species) %>%
-  summarise(sum_abundance = sum(pres_abs, na.rm = TRUE)) %>%
-  spread(species, sum_abundance)
+plant_data_cult <- longform_plant_percentcover_owen %>% filter(species %in% cult.imp.sp)
+
+
+
+plant_data_cult_wide <- plant_data_cult[,c(1:7)] %>% 
+  group_by(unq_tran,species) %>% 
+  summarise(cover_mean = mean(cover, na.rm=TRUE)) %>% 
+  spread(species, cover_mean)%>%  replace(is.na(.), 0)
+
+head(plant_data_cult_wide)
+
 # Create new DF for richness by node
-plant_data_cult_richness <- plant_data_cult_wide %>% select(node)
+plant_data_cult_richness <- plant_data_cult_wide %>% dplyr::select(unq_tran)
 # Add new col richness, dont wanna count node for sp (-1), will give 1 sp number per node
-plant_data_cult_richness$richness <- specnumber(plant_data_cult_wide[,-1])
+plant_data_cult_richness$cult_imp_plant_richness <- specnumber(plant_data_cult_wide[,-1])
 # Order nodes by richness
-plant_data_cult_richness$node <- factor(plant_data_cult_richness$node,
-                                        levels = plant_data_cult_richness$node[order(plant_data_cult_richness$richness)])
-# Get rid of NAs
-plant_data_cult_richness <- na.omit(plant_data_cult_richness)
+
+head(plant_data_cult_richness)
+
+write.csv(plant_data_cult_richness, "C:Biodiversity idea//Output files//plant_data_cult_richness.csv", row.names=FALSE)
+
