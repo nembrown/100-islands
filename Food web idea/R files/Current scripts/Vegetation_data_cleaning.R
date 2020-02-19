f8d21026fe8e3bbe39d20a826cd31588324dc87d
Norah Brown <norahbrown@uvic.ca>
2020-02-18 16:00:46 -0800

    Replaced the habitation file, called it vegetation cleaning, streamlined a lot of it. Took outthings that are not species. This file is ready for sp richness anlysis

--- Food web idea/R files/Current scripts/Vegetation_data_cleaning.R
+++ Food web idea/R files/Current scripts/Vegetation_data_cleaning.R
@@ -1,3 +1,6 @@
+#Cleaned up this file February 2020 - take out references to large and small - that can be computed later... 
+# Just combine Deb and Owen's plant information and extract out any marine information
+#take out bare ground as species
 
 library(tidyr)
 library(plyr)
@@ -10,68 +13,28 @@ library(ggplot2)
 
 
 # Read in Owen's data
-longform_plant_percentcover<-read.csv("Food web idea//Data by person//Owen's data//Complete_long_percentcover_mod.csv", header=TRUE, sep=",")
-
-#Read in Pat's data for area
-islands_data<-read.csv("Food web idea//Data by person//Pat.data//HabitatClass.csv", header=TRUE, sep=",")
-head(islands_data)
-
-names(islands_data)[1]<-"unq_isl"
-length(unique(islands_data$unq_isl))
-#100 islands
-
-#simply to just island and area
-islands_data<-islands_data[,c(1,4)]
-
-#how many islands does Owen have? 
-head(longform_plant_percentcover)
-length(unique(longform_plant_percentcover$unq_isl))
-#94 owen
-
-#merge owen and area
-islands_plant<-merge(longform_plant_percentcover, islands_data, by="unq_isl")
-head(islands_plant)
-
-#Label small islands as those with Area less than 6000m2
-levels <- c(-Inf, 6000, Inf)
-labels <- c("small", "large")
-islands_plant<- islands_plant %>% mutate(size.cat = cut(Area, levels, labels = labels))
-head(islands_plant)
+islands_plant<-read.csv("Food web idea//Data by person//Owen's data//Complete_long_percentcover_mod.csv", header=TRUE, sep=",")
 
 
-#Large islands are those that are over 6000m2, except for those that are long and skinny ... so max distance is less than 40
-islands_plant<- islands_plant  %>%group_by(unq_isl)%>%  mutate(large_dist= max(dist))
-islands_plant <- islands_plant %>% mutate(size.cat= replace(size.cat, large_dist>40, "small")) %>% ungroup()
-
-islands_plant<-islands_plant[!duplicated(islands_plant),]
-
-
-head(islands_plant)
-#summing across layers Owen but no T!!!! 
+#summing across layers Owen but no T, because T is summing across layers, so need to exclude
 islands_plant_noT <- islands_plant %>%  filter(layer!= "T")
 
 islands_plant_sum<- islands_plant_noT %>%  group_by(unq_plot, species)%>% summarise(cover =sum(cover))
-
+#This is summed per plot across all layers per species
 head(islands_plant_sum)
-islands_plant_sum[duplicated(islands_plant_sum),]
 
-islands_plant_info<-islands_plant[,-c(9:11, 14)]
-head(islands_plant_info)
-islands_plant_info<-islands_plant_info[!duplicated(islands_plant_info),]
-
+head(islands_plant_noT)
+islands_plant_noT_shore<-islands_plant_noT %>% dplyr::select(unq_isl, unq_plot, unq_tran, shore_dist)
+islands_plant_noT_shore<-islands_plant_noT_shore[!duplicated(islands_plant_noT_shore),]
 
 
-
-islands_plant_filtered<-inner_join(islands_plant_sum, islands_plant_info, by=c("unq_plot")) 
+islands_plant_filtered<-merge(islands_plant_sum, islands_plant_noT_shore, by="unq_plot", all.x=TRUE)
 head(islands_plant_filtered)
 
-length(unique(islands_plant_filtered$unq_isl))
-#94
 
-islands_plant_filtered[duplicated(islands_plant_filtered),]
+
 
 # Deb's data read and tidy ------------------------------------------------
-
 
 #read in Deb's data
 veg1x1_Deb_mod<-read.csv("Food web idea//Data by person//Owen's data//bird_long_percentcover_mod.csv", header=TRUE, sep=",")
@@ -87,35 +50,22 @@ names(veg1x1_Deb_mod_interior)[11]<-"shore_dist"
 length(unique(veg1x1_Deb_mod_interior$unq_isl))
 #99 islands 
 
+veg1x1_Deb_mod_interior_shore<-veg1x1_Deb_mod_interior %>% dplyr::select(unq_isl, unq_tran, shore_dist)
+veg1x1_Deb_mod_interior_shore<-veg1x1_Deb_mod_interior_shore[!duplicated(veg1x1_Deb_mod_interior_shore),]
+veg1x1_Deb_mod_interior_size<-veg1x1_Deb_mod_interior
 
-#which column of Owen's data is size.cat? 
-which( colnames(islands_plant)=="size.cat" )
-#column 16
-
-#adding island size.cat to Deb's data:
-veg1x1_Deb_mod_interior_size<-join(veg1x1_Deb_mod_interior,islands_plant[!duplicated(islands_plant$unq_isl), c(1,16 )], by="unq_isl")
-head(veg1x1_Deb_mod_interior_size)
-
-#make a transect that is the same as plot for Deb's (since each plot is it's own transect) 
-veg1x1_Deb_mod_interior_size$unq_plot<-veg1x1_Deb_mod_interior_size$unq_tran
-
-veg1x1_Deb_mod_interior_size<-veg1x1_Deb_mod_interior_size[!duplicated(veg1x1_Deb_mod_interior_size),]
-
-veg1x1_Deb_mod_interior_size_info<-veg1x1_Deb_mod_interior_size[,-c(7:10)]
-veg1x1_Deb_mod_interior_size_info<-veg1x1_Deb_mod_interior_size_info[!duplicated(veg1x1_Deb_mod_interior_size_info),]
-head(veg1x1_Deb_mod_interior_size_info)
 
 #summing across layers (e.g. canopy, herbs) Deb
 veg1x1_Deb_mod_interior_size_noT<-veg1x1_Deb_mod_interior_size %>% filter(layer!="T")
-veg1x1_Deb_mod_interior_size_sum<- veg1x1_Deb_mod_interior_size_noT %>%  group_by(unq_plot, species)%>% summarise(cover =sum(cover))
+veg1x1_Deb_mod_interior_size_sum<- veg1x1_Deb_mod_interior_size_noT %>%  group_by(unq_tran, species)%>% summarise(cover =sum(cover))
 head(veg1x1_Deb_mod_interior_size_sum)
 
 #add this back to the main dataframe without the "species, layer, cover, notes" categories
-veg1x1_Deb_mod_interior_size_filtered<-inner_join(veg1x1_Deb_mod_interior_size_sum,veg1x1_Deb_mod_interior_size_info,  by=c("unq_plot")) 
+#make a transect that is the same as plot for Deb's (since each plot is it's own transect) 
+veg1x1_Deb_mod_interior_size_sum$unq_plot<-veg1x1_Deb_mod_interior_size_sum$unq_tran
+
+veg1x1_Deb_mod_interior_size_filtered<-merge(veg1x1_Deb_mod_interior_size_sum, veg1x1_Deb_mod_interior_shore)
 head(veg1x1_Deb_mod_interior_size_filtered)
-length(unique(veg1x1_Deb_mod_interior_size_filtered$unq_isl))
-#99 islands 
-veg1x1_Deb_mod_interior_size_filtered[duplicated(veg1x1_Deb_mod_interior_size_filtered),]
 
 
 # Combining Owen and Deb --------------------------------------------------
@@ -125,69 +75,75 @@ veg1x1_Deb_mod_interior_size_filtered[duplicated(veg1x1_Deb_mod_interior_size_fi
 islands_plant_filtered$person<-"Owen"
 veg1x1_Deb_mod_interior_size_filtered$person<-"Deb"
 
-
-which( colnames(islands_plant_filtered)=="unq_isl" )
-which( colnames(islands_plant_filtered)=="size.cat" )
-which( colnames(islands_plant_filtered)=="unq_tran" )
-which( colnames(islands_plant_filtered)=="unq_plot" )
-which( colnames(islands_plant_filtered)=="shore_dist" )
-#which( colnames(islands_plant_filtered)=="layer" )
-which( colnames(islands_plant_filtered)=="species" )
-which( colnames(islands_plant_filtered)=="cover" )
-which( colnames(islands_plant_filtered)=="person" )
-
-
-which( colnames(veg1x1_Deb_mod_interior_size_filtered)=="unq_isl" )
-which( colnames(veg1x1_Deb_mod_interior_size_filtered)=="size.cat" )
-which( colnames(veg1x1_Deb_mod_interior_size_filtered)=="unq_tran" )
-which( colnames(veg1x1_Deb_mod_interior_size_filtered)=="unq_plot" )
-which( colnames(veg1x1_Deb_mod_interior_size_filtered)=="shore_dist" )
-#which( colnames(veg1x1_Deb_mod_interior_size_filtered)=="layer" )
-which( colnames(veg1x1_Deb_mod_interior_size_filtered)=="species" )
-which( colnames(veg1x1_Deb_mod_interior_size_filtered)=="cover" )
-which( colnames(veg1x1_Deb_mod_interior_size_filtered)=="person" )
-
-
-
 #combining deb and owen's data 
-Deb_Owen_veg_combined<-rbind(islands_plant_filtered[,c(4,14,12,1,11,2,3, 16)], veg1x1_Deb_mod_interior_size_filtered[,c(8,11,9,1,10,2,3,12)])
+Deb_Owen_veg_combined<-rbind(islands_plant_filtered, veg1x1_Deb_mod_interior_size_filtered)
 
 
 head(Deb_Owen_veg_combined)
 length(unique(Deb_Owen_veg_combined$unq_isl))
 #99
 
-#There are some islands in Deb's database but not Owen's... 
-which (is.na(Deb_Owen_veg_combined$size.cat))
+#Fixing some species
 
-#islands in Deb's dataset but not in Owens ... also TQ09 but can't find any info about that - discarded as an island? 
-#just did this by hand but could have done earlier in a better way (cutting area etc.. )
-Deb_Owen_veg_combined$size.cat[Deb_Owen_veg_combined$unq_isl=="ST05"]<-"large"
-Deb_Owen_veg_combined$size.cat[Deb_Owen_veg_combined$unq_isl=="PR12"]<-"small"
-Deb_Owen_veg_combined$size.cat[Deb_Owen_veg_combined$unq_isl=="PR07"]<-"small"
-Deb_Owen_veg_combined$size.cat[Deb_Owen_veg_combined$unq_isl=="MM09"]<-"small"
-Deb_Owen_veg_combined$size.cat[Deb_Owen_veg_combined$unq_isl=="MM10"]<-"large"
-Deb_Owen_veg_combined$size.cat[Deb_Owen_veg_combined$unq_isl=="MM11"]<-"large"
-Deb_Owen_veg_combined$size.cat[Deb_Owen_veg_combined$unq_isl=="TQ09"]<-"small"
-which (is.na(Deb_Owen_veg_combined$size.cat))
+Deb_Owen_veg_combined_wide <-Deb_Owen_veg_combined %>%  group_by(unq_plot) %>% spread(species, cover)
 
-head(Deb_Owen_veg_combined)
-Deb_Owen_veg_combined<-Deb_Owen_veg_combined[!duplicated(Deb_Owen_veg_combined),]
+head(Deb_Owen_veg_combined_wide)
+longform_plant_percentcover2<-Deb_Owen_veg_combined_wide
 
-# Filling 0s for each species ---------------------------------------------
+#combining some species
+longform_plant_percentcover2$marine<-longform_plant_percentcover2$`o soil`+ longform_plant_percentcover2$`marine remains`+longform_plant_percentcover2$`abalone shell`+longform_plant_percentcover2$driftwood+longform_plant_percentcover2$shell
+longform_plant_percentcover2$free_space<-longform_plant_percentcover2$bare+longform_plant_percentcover2$`bare ground`+longform_plant_percentcover2$`sandy soil`+longform_plant_percentcover2$`o soil`+longform_plant_percentcover2$gravel+longform_plant_percentcover2$rock
+longform_plant_percentcover2$grass<-longform_plant_percentcover2$`grass 1`+longform_plant_percentcover2$`grass sp`
+longform_plant_percentcover2$sedge_final<-longform_plant_percentcover2$`sedge 1`+longform_plant_percentcover2$`sedge sp`+longform_plant_percentcover2$sedge+longform_plant_percentcover2$sedge1
+longform_plant_percentcover2$unknown_forb<-longform_plant_percentcover2$'unk forb'+ longform_plant_percentcover2$'unidentified forb'
+longform_plant_percentcover2$unknown_lily<-longform_plant_percentcover2$'unk lily'+ longform_plant_percentcover2$'unk lily sp'+longform_plant_percentcover2$'lily seedling'
+longform_plant_percentcover2$unknown_monocot<-longform_plant_percentcover2$'unk mono'+ longform_plant_percentcover2$'unk monocot'
+ 
 
+#take out the marine indicators, things that are not plants, and the duplicates for the composite variables above.... just for species richness calculations
+ not_species_names<-c("marine" ,
+                      "free_space" ,
+                      "bare" ,
+                      "bare ground" ,
+                      "woody debris" ,
+                      "wood" ,
+                      "sandy soil" ,
+                      "o soil" ,
+                      "shell" ,
+                      "gravel" ,
+                      "rock" ,
+                      "marine remains" ,
+                      "abalone shell" ,
+                      "driftwood" ,
+                      "feather" ,
+                      "grass 1" ,
+                      "grass sp" ,
+                      "sedge" ,
+                      "sedge1" ,
+                      "sedge 1" ,
+                      "sedge sp" ,
+                      "unk forb" ,
+                      "unidentified forb",
+                      "unk lily" ,
+                      "lily seedling" ,
+                      "unk lily sp" ,
+                      "unk mono" ,
+                      "unk monocot")
+ 
+longform_plant_percentcover_species<-longform_plant_percentcover2[, ! colnames(longform_plant_percentcover2) %in% not_species_names]
+head(longform_plant_percentcover_species)
 
-Deb_Owen_veg_combined_complete <- Deb_Owen_veg_combined%>% ungroup() %>% 
-                                    complete(unq_plot, species, fill=list(cover=0)) %>% 
-                                    group_by(unq_plot) %>% 
-                                    arrange(desc(cover), .by_group=TRUE)
+longform_plant_percentcover_species_long<-longform_plant_percentcover_species %>% group_by(unq_plot) %>% gather(species, cover, 6:154)
+head(longform_plant_percentcover_species_long)
+#lots of Nas but we will deal with thme next
 
-Deb_Owen_veg_combined_complete_filled<-Deb_Owen_veg_combined_complete %>% fill(everything())
+Deb_Owen_veg_combined_complete_filled<-longform_plant_percentcover_species_long
 
-View(Deb_Owen_veg_combined_complete_filled)
+Deb_Owen_veg_combined_complete_filled[is.na(Deb_Owen_veg_combined_complete_filled)] <- 0
+
 
 length(unique(Deb_Owen_veg_combined_complete_filled$unq_isl))
-#100
+#99
 
 ##adding in info about shurb herb or tree
 plant_category<-read.csv("Food web idea//Data by person//Owen's data//100Islands_Fitzpatrick_species.csv", header=TRUE, sep=",")
@@ -200,7 +156,31 @@ Deb_Owen_veg_combined_complete_filled<-merge(Deb_Owen_veg_combined_complete_fill
 head(Deb_Owen_veg_combined_complete_filled)
 
 
-write.csv(Deb_Owen_veg_combined_complete_filled, "Food web idea//Data by person//Kalina.data/Deb_Owen_veg_combined_complete_filled.csv")
+write.csv(Deb_Owen_veg_combined_complete_filled, "Food web idea//Data by person//Kalina.data/Deb_Owen_veg_combined_complete_filled.csv", row.names=FALSE)
+#### This is complete with no "bad" species- all are plnats, no bare ground etc.... 
+
+
+
+###Getting a vector of just marine stuff
+
+head(longform_plant_percentcover2)
+
+marine_by_plot_from_plants<-longform_plant_percentcover2 %>% dplyr::select(unq_plot, unq_isl, unq_tran, shore_dist, person, marine)
+head(marine_by_plot_from_plants)
+
+
+
+write.csv(marine_by_plot_from_plants, "Food web idea//Data by person//Norah.data/marine_by_plot_from_plants.csv", row.names=FALSE)
+
+
+
+
+
+####################### below here I have left the same
+
+
+
+
 ### add in coordinates
 owen_coords<-read.csv("C:Food web idea//Data by person//Becky.data//ofwi_tran_coords_mod_2.csv", header=TRUE, sep=",")
 owen_coords<-owen_coords[,c(1:9)]
