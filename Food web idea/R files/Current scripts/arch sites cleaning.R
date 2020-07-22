36b6fb508737a2f9c4079c4e80d8e0ca6233756a
Norah Brown <norahbrown@uvic.ca>
2020-07-22 10:21:54 -0700

    Moved some code here from biodiversity_idea_master script, presence absence for arch sites

--- Food web idea/R files/Current scripts/arch sites cleaning.R
+++ Food web idea/R files/Current scripts/arch sites cleaning.R
@@ -17,3 +17,59 @@ arch_sites_selected<- arch_sites %>% dplyr::select(BORDENNUMBER, MR_GISUTMEASTIN
 head(arch_sites_selected)
 
 write.csv(arch_sites_selected, "C:Biodiversity idea//Output files//arch_sites_selected.csv", row.names=FALSE)
+
+
+
+
+
+
+
+
+#### sode code for combing arch sites:
+#arch sites paired
+arch_sites_distance_tran<-read.csv("Biodiversity idea//Output files//paired_arch_by_radius_300.csv")
+head(arch_sites_distance_tran)
+length(unique(arch_sites_distance_tran$unq_tran))
+#84 unique transects if using 300m radius 
+
+##adding in arch data from output file fed from arch sites cleaning.R
+arch_data<-read.csv("C:Biodiversity idea//Output files//arch_sites_selected.csv")
+head(arch_data)
+arch_data_simple<-arch_data[ , c("site_id", "CMT", "clam_garden", "midden_feature", "fish_feature", "canoe_skid")]
+head(arch_data_simple)
+
+# arch_merged<-merge(arch_sites_distance_tran, arch_data_simple, by="site_id", all=TRUE)
+# head(arch_merged)
+# 
+# fish_richness_merged_tran_arch<-merge(fish_richness_merged_tran, arch_sites_distance_tran, by="unq_tran", all.x=TRUE)
+# 
+# #head(fish_richness_merged_tran_arch)
+# length(unique(fish_richness_merged_tran_arch$unq_tran))
+# 
+# 
+# fish_richness_merged_tran_arch<-merge(fish_richness_merged_tran_arch, arch_data_simple, by="site_id", all.x=TRUE)
+# View(fish_richness_merged_tran_arch)
+
+# #for sem:
+# fish_richness_merged_tran_arch$midden_feature_sem<-as.character(fish_richness_merged_tran_arch$midden_feature)
+# fish_richness_merged_tran_arch$midden_feature_sem<- dplyr::recode(fish_richness_merged_tran_arch$midden_feature_sem, yes = "1", no="0")
+# fish_richness_merged_tran_arch$midden_feature_sem[is.na(fish_richness_merged_tran_arch$midden_feature_sem)] <- 0
+# fish_richness_merged_tran_arch$midden_feature_sem<-as.numeric(fish_richness_merged_tran_arch$midden_feature_sem)
+# 
+# fish_richness_merged_tran_arch$fish_feature_sem<-as.character(fish_richness_merged_tran_arch$fish_feature)
+# fish_richness_merged_tran_arch$fish_feature_sem<-dplyr::recode(fish_richness_merged_tran_arch$fish_feature_sem, yes = "1", no="0")
+# fish_richness_merged_tran_arch$fish_feature_sem[is.na(fish_richness_merged_tran_arch$fish_feature_sem)] <- 0
+# fish_richness_merged_tran_arch$fish_feature_sem<-as.numeric(fish_richness_merged_tran_arch$fish_feature_sem)
+# 
+# fish_richness_merged_tran_arch$canoe_skid_sem<-as.character(fish_richness_merged_tran_arch$canoe_skid)
+# fish_richness_merged_tran_arch$canoe_skid_sem<-dplyr::recode(fish_richness_merged_tran_arch$canoe_skid_sem, yes = "1", no="0")
+# fish_richness_merged_tran_arch$canoe_skid_sem[is.na(fish_richness_merged_tran_arch$canoe_skid_sem)] <- 0
+# fish_richness_merged_tran_arch$canoe_skid_sem<-as.numeric(fish_richness_merged_tran_arch$canoe_skid_sem)
+# 
+# 
+# fish_richness_merged_tran_arch$CMT<-as.factor(fish_richness_merged_tran_arch$CMT)
+# fish_richness_merged_tran_arch$clam_garden<-as.factor(fish_richness_merged_tran_arch$clam_garden)
+# fish_richness_merged_tran_arch$midden_feature<-factor(fish_richness_merged_tran_arch$midden_feature)
+# fish_richness_merged_tran_arch$fish_feature<-as.factor(fish_richness_merged_tran_arch$fish_feature)
+# fish_richness_merged_tran_arch$canoe_skid<-as.factor(fish_richness_merged_tran_arch$canoe_skid)
+# 
