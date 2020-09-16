82bf77815eacaa491c848edaf97d9da4029f0b7c
Norah Brown <norahbrown@uvic.ca>
2020-09-16 16:47:32 -0700

    used 101 islands.... estimated missingness with amelia

--- Food web idea/R files/Current scripts/SEM_lavaan_d15n_richness_island.R
+++ Food web idea/R files/Current scripts/SEM_lavaan_d15n_richness_island.R
@@ -45,11 +45,9 @@ head(master_island_sem_subset_richness_island_centered)
 summary(master_island_sem_subset_richness_island_centered)
 md.pattern(master_island_sem_subset_richness_island_centered)
 
-master_island_sem_subset_richness_island_centered<- master_island_sem_subset_richness_island_centered[complete.cases(master_island_sem_subset_richness_island_centered$c.log_MEAN_kparea2k), ]
-master_island_sem_subset_richness_island_centered<- master_island_sem_subset_richness_island_centered[complete.cases(master_island_sem_subset_richness_island_centered$otter_pres), ]
-
 # Simple non-hierarchical model  -------------------------------------------------------
 #Will combine this with survey.design to get hierachical model
+
 
 
 N15_model_simple_centered_alt_richness_island<-'
@@ -64,42 +62,59 @@ N15_model_simple_centered_alt_richness_island<-'
         
         eagles ~  c.log_fish_biomass_bym3_mean + c.log_bycatch_biomass_bym3_mean + c.log_Area + c.PA_norml + c.log_MEAN_kparea2k + c.log_MEAN_egarea2k + c.log_MEAN_rockarea2000 + elevation_max + c.CHM_mean_height
 
-        c.log_site_mean_by_isl ~  c.log_MEAN_kparea2k + c.log_MEAN_egarea2k + c.SLOPE_degrees  + c.WAVE_EXPOSURE + beachy_substrate + c.log_Area + c.PA_norml  + c.log_MEAN_rockarea2000
+        c.log_site_mean_by_isl ~  c.log_MEAN_kparea2k + c.log_MEAN_egarea2k + c.SLOPE_degrees  + c.WAVE_EXPOSURE + beachy_substrate + c.log_Area + c.PA_norml  + c.log_MEAN_rockarea2000  + c.slope_isl
 
         c.distance_to_midden ~ c.log_Dist_Near + c.log_MEAN_kparea2k + c.log_MEAN_egarea2k + c.log_MEAN_rockarea2000 + c.log_fish_biomass_bym3_mean + c.log_bycatch_biomass_bym3_mean  + c.WAVE_EXPOSURE + c.log_Area  + c.PA_norml + beachy_substrate + elevation_max 
 
-        c.d15n ~  w1*c.log_site_mean_by_isl + h1*c.distance_to_midden + c.WAVE_EXPOSURE + otter_pres + ravens + eagles + elevation_max + c.slope_isl  + c.log_Bog_area + c.CHM_mean_height
+        c.d15n ~  w1*c.log_site_mean_by_isl + h1*c.distance_to_midden + c.WAVE_EXPOSURE + otter_pres + ravens + eagles + elevation_max + c.slope_isl  + c.log_Bog_area + c.CHM_mean_height + c.log_Area
 
         c.CHM_mean_height ~ c.log_Area + elevation_max + c.PA_norml + c.slope_isl + c.WAVE_EXPOSURE + c.log_Bog_area 
-
-        c.plant_richness ~ otter_pres + c.d15n + c.log_Area + elevation_max + c.PA_norml + c.slope_isl + c.WAVE_EXPOSURE + c.distance_to_midden + c.log_DistW_ML'
+        
+        c.plant_richness ~ otter_pres + c.d15n + c.log_Area + elevation_max + c.PA_norml + c.slope_isl + c.WAVE_EXPOSURE + c.distance_to_midden + c.log_DistW_ML
+        
+        c.PA_norml ~ c.log_Area '
 
 
 
 # Multiple imputation -----------------------------------------------------
-# We need to use multiple imputation because the methods to understand our multi-level data dont accept missing data
+
 
 # # Amelia ---------------------------------------------------------
 # set.seed(12345)
-# HS.amelia_richness_island <- amelia(master_island_sem_subset_richness_island_centered, m = 20, idvars = c( "unq_isl", "node"), p2s = FALSE)
-# imps_richness_island <- HS.amelia_richness_island$imputations
-# imps_amelia_richness_island<-imputationList(imps_richness_island)
+HS.amelia_richness_island <- amelia(master_island_sem_subset_richness_island_centered, m = 20, idvars = c( "unq_isl", "node"), p2s = FALSE)
+imps_richness_island <- HS.amelia_richness_island$imputations
+imps_amelia_richness_island<-imputationList(imps_richness_island)
 
-# Will not impute, will just use complete cases
 
-# Running model and adding survey design ----------------------------------
+# Running model four options----------------------------------
 
-#run basic empty model then update with survey design
+#run basic model with FIML
+missing_fit_model_richness_island<-sem(N15_model_simple_centered_alt_richness_island, data=master_island_sem_subset_richness_island_centered, missing="fiml")
+summary(missing_fit_model_richness_island, standardized=T)
+fitMeasures(missing_fit_model_richness_island, c("cfi","rmsea","srmr"))
+
+#run amelia imputation (single level) then then model
+amelia_fit_model_richness_island<-semList(N15_model_simple_centered_alt_richness_island, dataList=imps_richness_island)
+summary(amelia_fit_model_richness_island)
+fitMeasures(amelia_fit_model_richness_island, c("cfi","rmsea","srmr"))
+
+#run amelia and the model simultaenously using runMI
+fit.mi.amelia<-runMI(N15_model_simple_centered_alt_richness_island,master_island_sem_subset_richness_island_centered, fun = "sem",m=20, miArgs=list(idvars = c("unq_isl", "node")),
+                     miPackage = "Amelia", seed = 12345)
+summary(fit.mi.amelia)
+fitMeasures(fit.mi.amelia, c("cfi","rmsea","srmr"))
+
+#Survey.design
 lavaan_fit_model_richness_island<-sem(N15_model_simple_centered_alt_richness_island, data=master_island_sem_subset_richness_island_centered)
-summary(lavaan_fit_model_richness_island, standardized=T)
+design_imp_amelia_richness_island<-svydesign(ids=~unq_isl, strata=~node, data=imps_amelia_richness_island)
+fit.adj.richness_island<-lavaan.survey(lavaan.fit=lavaan_fit_model_richness_island, survey.design = design_imp_amelia_richness_island, estimator="MLMVS")
+summary(fit.adj.richness_island, standardized=T)
+fitMeasures(fit.adj.richness_island, c("cfi","rmsea","srmr"))
 
-# # Survey.design
-# design_richness_island<-svydesign(ids=~node, data=master_island_sem_subset_richness_island_centered)
-# 
-# fit.adj.richness<-lavaan.survey(lavaan.fit=lavaan_fit_model_richness_island, survey.design = design_richness_island, estimator="MLMVS")
-# summary(fit.adj.richness, standardized=T)
+# #estimator="MLMVS"
+# survey design doesn't work with Node - since not all nodes have enough to be considered
 
-plyr::arrange(modificationIndices(lavaan_fit_model_richness_island),mi, decreasing=TRUE)
+plyr::arrange(modificationIndices(fit.adj.richness_island),mi, decreasing=TRUE)
 #######
 
 #semplot
@@ -113,7 +128,7 @@ grps_richness_island<-list(Algae=c("c.log_MEAN_rockarea2000","c.log_site_mean_by
 colour_group=c("#51C5B4", "#C2B3A2", "#EC6D98", "#539E59", "#00A4EE", "#CD8958")
 
 nodelab_richness_island<-c("fish", "marine\ninvert","otter", "ravens", "eagles" ,"wrack", "distance\nmidden", "d15N\nsoil",  "canopy\nheight","plant\nrichness", 
-               "kelp", "eelgrass", "fucus", "sandy", "Area","edge\neffects", "neighb","beach\nslope",  "wave\nexposure","elevation",
+                           "edge\neffects","kelp", "eelgrass", "fucus", "sandy", "Area", "neighb","beach\nslope",  "wave\nexposure","elevation",
                "island\nslope","Bog\narea", "Dist\nmainland")
 
 
@@ -121,34 +136,38 @@ lay_names_richness_island<-get_layout(
                           "", "", "", "", "", "","","","","","","","","","","","","","","","","","","","","","","","","","","","", "","", "","","","", "","","","","", "","","","","", "","","","","", "plant\nrichness","","","","","","","","","","","","","","","","","","","","","","","","","","",
                           "","","","","","","","","","","","","","","","","","","","","","","","", "","", "","","","", "","","","d15N\nsoil",  "","","", "", "", "", "", "","","","", "","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","",
                           "wrack","","", "","","","","", "","","","","","", "","", "","","","","","","", "", "","", "","","","","","","","","","","","","","","","", "","","","","", "","","","", "","","","","","","","","","","","","","","","","","","","","","","","","","","","","","",
-                          "","", "","","","","","","","","","", "eagles","","","","","","","ravens" ,"","","","","","","otter","","","","","","","","","","","","","","","","","","", "distance\nmidden","","","","","","","","","","","","","","","","","","","", "","","","","","","","","","","","","","","","",
-                          "","","","","","","","","","","","","","","marine\ninvert","","","","","","","","","","", "fish","","","","","","","","","","","", "","","","","","","","","","","","","","","","","","","","","","","canopy\nheight","","","","","","","","","","","","","","","","","","","","","",
-                          "fucus","","","", "","","kelp","","","", "","","eelgrass","","","","","","","beach\nslope","","","","","","","wave\nexposure","","","","","","", "sandy","","","","","","","", "Dist\nmainland","","","","","","","","","edge\neffects","","","","","","Area","","","", "","","Bog\narea","","","","","", "neighb","","","","","", "elevation", "","","","","", "island\nslope", rows=6)
+                          "","", "","","","","","","","","","", "eagles","","","","","","","ravens" ,"","","","","","","otter","","","","","","","","","","","","", "distance\nmidden","","","","","","","","","","","","","","","","","","","","","","","","","", "","","","","","","","","","","","","","","","",
+                          "","","","","","","","","","","","","","","marine\ninvert","","","","","","","","","","", "fish","","","","","","","","","","","", "","","","","","","","","","","","","","","","","","","","","","edge\neffects","","","","","","","","","canopy\nheight","","","","","","","","","","","","","",
+                          "fucus","","","", "","","kelp","","","", "","","eelgrass","","","","","","","beach\nslope","","","","","","","wave\nexposure","","","","","","", "sandy","","","","","","","", "Dist\nmainland","","","","","","","","","","","","","","","Area","","","", "","","Bog\narea","","","","","", "neighb","","","","","", "elevation", "","","","","", "island\nslope", rows=6)
 
 semPaths(lavaan_fit_model_richness_island, what="std",  layout=lay_names_richness_island, intercepts=FALSE, residuals=FALSE,
          groups=grps_richness_island, exoVar = FALSE,  color=colour_group, esize=2, nodeLabels = nodelab_richness_island, legend=FALSE)
+
+
+semPaths(fit.adj.richness_island, what="std",  layout=lay_names_richness_island, intercepts=FALSE, residuals=FALSE,
+         groups=grps_richness_island, exoVar = FALSE,  color=colour_group, esize=2, nodeLabels = nodelab_richness_island, legend=FALSE)
+
+
 
 semPaths(lavaan_fit_model_richness_island, what="path",  layout=lay_names_richness_island, intercepts=FALSE, residuals=FALSE,
          groups=grps, exoVar = FALSE, color=colour_group, nodeLabels = nodelab_richness_island, legend=FALSE)
 
 
-fit <- lavaan_fit_model_richness_island
+fit_isl <- fit.adj.richness_island
 
-lavaan::standardizedSolution(fit) %>% dplyr::filter(!is.na(pvalue)) %>% arrange(desc(pvalue)) %>% mutate_if("is.numeric","round",3) %>% select(-ci.lower,-ci.upper,-z)
+lavaan::standardizedSolution(fit_isl) %>% dplyr::filter(!is.na(pvalue)) %>% arrange(desc(pvalue)) %>% mutate_if("is.numeric","round",3) %>% select(-ci.lower,-ci.upper,-z)
 
 pvalue_cutoff <- 0.10
 
-obj_isl <- semPlot:::semPlotModel(fit)
+obj_isl <- semPlot:::semPlotModel(fit_isl)
 
 # save a copy of the original, so we can compare it later and be sure we removed only what we intended to remove
 original_Pars_isl <- obj_isl@Pars
 
 check_Pars_isl <- obj_isl@Pars %>% dplyr::filter(!(edge %in% c("int","<->") | lhs == rhs)) # this is the list of paramater to sift thru
 keep_Pars_isl <- obj_isl@Pars %>% dplyr::filter(edge %in% c("int","<->") | lhs == rhs) # this is the list of paramater to keep asis
-test_against <- lavaan::standardizedSolution(fit) %>% dplyr::filter(pvalue < pvalue_cutoff, rhs != lhs)
-test_against_rev <- test_against %>% rename(rhs2 = lhs,   # for some reason, the rhs and lhs are reversed in the standardizedSolution() output, for some of the values
-                                            lhs = rhs) %>% # I'll have to reverse it myself, and test against both orders
-  rename(rhs = rhs2)
+test_against <- lavaan::standardizedSolution(fit_isl) %>% dplyr::filter(pvalue < pvalue_cutoff, rhs != lhs)
+test_against_rev <- test_against %>% dplyr::rename(rhs2 = lhs, lhs = rhs) %>% dplyr::rename(rhs = rhs2)
 checked_Pars_isl <-
   check_Pars_isl %>% semi_join(test_against, by = c("lhs", "rhs")) %>% bind_rows(
     check_Pars_isl %>% semi_join(test_against_rev, by = c("lhs", "rhs"))
@@ -166,6 +185,6 @@ semPaths(obj_isl, what="std",  layout=lay_names_richness_island, intercepts=FALS
          groups=grps_richness_island, exoVar = FALSE,  color=colour_group, esize=2, nodeLabels = nodelab_richness_island, legend=FALSE)
 
 
-semPlot::semPaths(lavaan_fit_model_richness_island, "path",fade = F, residuals = F, intercepts=FALSE, label.cex=2, nCharNodes = 0, nodeLabels = 1:23)
-semPlot::semPaths(lavaan_fit_model_richness_island, "path",fade = F, residuals = F, intercepts=FALSE, label.cex=2, nCharNodes = 0)
+semPlot::semPaths(fit.adj.richness_island, "path",fade = F, residuals = F, intercepts=FALSE, label.cex=2, nCharNodes = 0, nodeLabels = 1:23)
+semPlot::semPaths(fit.adj.richness_island, "path",fade = F, residuals = F, intercepts=FALSE, label.cex=2, nCharNodes = 0)
 
