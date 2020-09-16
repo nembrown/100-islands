cfe47a3ca8a675a1e394e28ff51402404bff65ff
Norah Brown <norahbrown@uvic.ca>
2020-09-16 16:47:06 -0700

    took out unnecessary lines only 50 islands matched with fish data so dealt with missingness

--- Biodiversity idea/R files/Current scripts/Biodiversity_idea_master_island.R
+++ Biodiversity idea/R files/Current scripts/Biodiversity_idea_master_island.R
@@ -34,9 +34,9 @@ head(fish_bycatch_richness_merged_isl)
 fish_bycatch_richness_merged_isl$unq_isl<-strtrim(fish_bycatch_richness_merged_isl$unq_tran, 4)
 
 ### averaging across islands
-# I didn't recalculate the radius for the centre of the island so as to not bias the big vs. small islands, since a big island with a center gps point would have way less water area
 fish_bycatch_richness_merged_isl <- fish_bycatch_richness_merged_isl %>% group_by(unq_isl) %>% summarise_if(is.numeric, mean, na.rm=TRUE)
 length(fish_bycatch_richness_merged_isl$unq_isl)
+#50 islands match with fish data
 
 ###FISH with unq_tran added but no transect data NO UNQ_ISL
 write.csv(fish_bycatch_richness_merged_isl, "C:Biodiversity idea//Output files//fish_bycatch_richness_merged_isl.csv", row.names=FALSE)
@@ -77,7 +77,7 @@ head(by_isl_master)
 by_isl_master$log_Area<-log(by_isl_master$Area)
 
 #head(fish_richness_merged_isl_arch)
-fish_richness_merged_isl_arch_2 <-merge(fish_richness_merged_isl_arch, by_isl_master, by="unq_isl", all.x=TRUE)
+fish_richness_merged_isl_arch_2 <-merge(fish_richness_merged_isl_arch, by_isl_master, by="unq_isl", all=TRUE)
 fish_richness_merged_isl_arch_2$log_site_mean_by_isl <- log(fish_richness_merged_isl_arch_2$site_mean_by_isl+1)
 fish_richness_merged_isl_arch_2$log_MEAN_kparea2k <- log(fish_richness_merged_isl_arch_2$MEAN_kparea2k+1)
 fish_richness_merged_isl_arch_2$log_MEAN_egarea2k <- log(fish_richness_merged_isl_arch_2$MEAN_egarea2k+1)
@@ -100,160 +100,5 @@ master_island<-fish_richness_merged_isl_arch_2
 write.csv(master_island, "C:Biodiversity idea//Output files//master_island.csv", row.names=FALSE)
 
 head(master_island)
-
-
-
-hist(fish_richness_merged_isl_arch_2$plot_cc)
-
-master_transect$slope_isl
-###########################################################
-
-
-
-
-
-
-
-
-
-
-plot(master_transect$log_Area~ log(master_transect$Bog_area+1))
-hist(master_transect$site_mean_by_tran, breaks=100)
-
-
-
-master_transect<- master_transect[complete.cases(master_transect$node), ]
-
-master_transect$super_node<-master_transect$node
-master_transect$super_node[master_transect$node=="PR"]<-"SCPR"
-master_transect$super_node[master_transect$node=="SC"]<-"SCPR"
-master_transect$super_node[master_transect$node=="TB"]<-"TBADMMGS"
-master_transect$super_node[master_transect$node=="AD"]<-"TBADMMGS"
-master_transect$super_node[master_transect$node=="MM"]<-"TBADMMGS"
-master_transect$super_node[master_transect$node=="GS"]<-"TBADMMGS"
-# master_transect$super_node[master_transect$node=="TQ"]<-"TQSTCV"
-# master_transect$super_node[master_transect$node=="ST"]<-"TQSTCV"
-# master_transect$super_node[master_transect$node=="CV"]<-"TQSTCV"
-
-master_transect$super_node
-
-
-
-# unq_isl -----------------------------------------------------------------
-
-
-#adding in a few interesting island-level components
-by_isl_master<-read.csv("C:Food web idea//Data by person//Owen's data//by_isl_master.csv")
-head(by_isl_master)
-by_isl_master_col_names<-c("unq_isl",
-       "mammal_richness",
-       "total_richness",
-       "bird.richness",
-       "bird.density",
-       "habitat_het",
-       "log_Area",
-       "Neighb_250",
-       "NDVI_mean",
-       "Perimeter",
-       "PA_norml",
-       "DistW_ML",
-       "Dist_Near",
-       "Area",
-       "size.cat2",
-       "eagles",
-       "ravens",
-       "node",
-       "SLOPE",
-       "slope_mean",
-       "Radius_m_2000",
-       "sum_2km",
-       "SITE_SUM",
-       "WAVE_EXPOSURE" )
-
-by_isl_master_subset<-by_isl_master[,colnames(by_isl_master) %in% by_isl_master_col_names]
-###head(by_isl_master_subset)
-
-###head(fish_bycatch_richness_merged_isl)
-fish_bycatch_richness_merged_isl$unq_isl<-str_sub(fish_bycatch_richness_merged_isl$unq_tran, start=0, end=4)
-
-##head(fish_bycatch_richness_merged_isl)
-fish_bycatch_richness_merged_isl <-fish_bycatch_richness_merged_isl %>% group_by(unq_isl) %>%
-  summarise_if(is.numeric, mean, na.rm=TRUE)
-
-
-fish_richness_merged_isl<-merge(fish_bycatch_richness_merged_isl, by_isl_master, by="unq_isl", all.y=TRUE)
-##head(fish_richness_merged_isl)
-length(unique(fish_richness_merged_isl$unq_isl))
-#102 islands
-
-xs4<- quantile(na.omit(fish_richness_merged_isl$fish_biomass_bym3_mean),c(0,0.25,0.75, 1))
-labels4 <- c("low fish biomass", "med fish biomass", "high fish biomass")
-fish_richness_merged_isl<- fish_richness_merged_isl %>% 
-  mutate(fish_biomass_bym3_cat_isl = cut(fish_biomass_bym3_mean, xs4, labels = labels4))
-
-xs_N<- quantile(na.omit(fish_richness_merged_isl$d15n),c(0,0.25,0.75, 1))
-labels_N <- c("low N15", "med N15", "high N15")
-fish_richness_merged_isl<- fish_richness_merged_isl %>% 
-  mutate(d15n_cat_isl = cut(d15n, xs_N, labels = labels_N))
-
-#d34S isn't included at the island level.... 
-xs_S<- quantile(na.omit(fish_richness_merged_isl$d34s),c(0,0.25,0.75, 1))
-labels_S <- c("low S34", "med S34", "high S34")
-fish_richness_merged_isl<- fish_richness_merged_isl %>% 
-  mutate(d34s_cat_isl = cut(d34s, xs_S, labels = labels_S))
-
-
-fish_richness_merged_isl$combined_richness_corrected<-fish_richness_merged_isl$wrack_richness+fish_richness_merged_isl$marine_richness_corrected
-fish_richness_merged_isl$eelgrass_cover_2km<-(fish_richness_merged_isl$MEAN_egarea2k)/(fish_richness_merged_isl$Radius_m_2000)
-fish_richness_merged_isl$habitat_cover_2km<-(fish_richness_merged_isl$sum_2km)/(fish_richness_merged_isl$Radius_m_2000)
-fish_richness_merged_isl$d15n_over_N<-(fish_richness_merged_isl$d15n)/(fish_richness_merged_isl$n)
-
-  
-  
-xs_habcover<- quantile(na.omit(fish_richness_merged_isl$habitat_cover_2km),c(0,0.25,0.75, 1))
-labels_habcover <- c("low habitat cover", "med habitat cover", "high habitat cover")
-fish_richness_merged_isl<- fish_richness_merged_isl %>% 
-  mutate(habcover_cat_isl = cut(habitat_cover_2km, xs_habcover, labels = labels_habcover))
-
-
-xs_fish.richness<- quantile(na.omit(fish_richness_merged_isl$fish_richness_corrected),c(0,0.25,0.75, 1))
-labels_fish.richness <- c("low fish.richness", "med fish.richness", "high fish.richness")
-fish_richness_merged_isl<- fish_richness_merged_isl %>% 
-  mutate(fish.richness_cat_isl = cut(fish_richness_corrected, xs_fish.richness, labels = labels_fish.richness))
-
-xs_wrack.richness<- quantile(na.omit(fish_richness_merged_isl$wrack_richness),c(0,0.25,0.75, 1))
-fish_richness_merged_isl$wrack.richness_cat_isl[fish_richness_merged_isl$wrack_richness==0]<-"no wrack"
-fish_richness_merged_isl$wrack.richness_cat_isl[fish_richness_merged_isl$wrack_richness>0 & fish_richness_merged_isl$wrack_richness<10]<-"low wrack richness"
-fish_richness_merged_isl$wrack.richness_cat_isl[fish_richness_merged_isl$wrack_richness>10]<-"high wrack richness"
-
-xs_SITE_SUM<- quantile(na.omit(fish_richness_merged_isl$SITE_SUM),c(0,0.25,0.75, 1))
-fish_richness_merged_isl$SITE_SUM_cat_isl[fish_richness_merged_isl$SITE_SUM==0]<-"no wrack"
-fish_richness_merged_isl$SITE_SUM_cat_isl[fish_richness_merged_isl$SITE_SUM>0 & fish_richness_merged_isl$SITE_SUM<169]<-"low beach wrack"
-fish_richness_merged_isl$SITE_SUM_cat_isl[fish_richness_merged_isl$SITE_SUM>169]<-"high beach wrack"
-
-fish_richness_merged_isl$bird.density<-as.numeric(as.character(fish_richness_merged_isl$bird.density))
-xs_bird.density<- quantile(na.omit(fish_richness_merged_isl$bird.density),c(0,0.25,0.75, 1))
-fish_richness_merged_isl$bird.density_cat_isl[fish_richness_merged_isl$bird.density<13]<-"low bird density"
-fish_richness_merged_isl$bird.density_cat_isl[fish_richness_merged_isl$bird.density>13 & fish_richness_merged_isl$bird.density<26]<-"med bird density"
-fish_richness_merged_isl$bird.density_cat_isl[fish_richness_merged_isl$bird.density>26]<-"high bird density"
-
-
-xs_schooling_fish_biomass_bym3_mean<- quantile(na.omit(fish_richness_merged_isl$schooling_fish_biomass_bym3_mean),c(0,0.25,0.75, 1))
-labels_schooling_fish_biomass_bym3_mean <- c("low schooling fish biomass", "med schooling fish biomass", "high schooling fish biomass")
-fish_richness_merged_isl<- fish_richness_merged_isl %>% 
-  mutate(schooling_fish_biomass_bym3_mean_cat_isl = cut(schooling_fish_biomass_bym3_mean, xs_schooling_fish_biomass_bym3_mean, labels = labels_schooling_fish_biomass_bym3_mean))
-
-xs_individual_fish_biomass_bym3_mean<- quantile(na.omit(fish_richness_merged_isl$individual_fish_biomass_bym3_mean),c(0,0.25,0.75, 1))
-labels_individual_fish_biomass_bym3_mean <- c("low individual fish biomass", "med individual fish biomass", "high individual fish biomass")
-fish_richness_merged_isl<- fish_richness_merged_isl %>% 
-  mutate(individual_fish_biomass_bym3_mean_cat_isl = cut(individual_fish_biomass_bym3_mean, xs_individual_fish_biomass_bym3_mean, labels = labels_individual_fish_biomass_bym3_mean))
-
-xs_bycatch_biomass_bym3_mean<- quantile(na.omit(fish_richness_merged_isl$bycatch_biomass_bym3_mean),c(0,0.25,0.75, 1))
-labels_bycatch_biomass_bym3_mean <- c("low invert biomass", "med invert biomass", "high invert biomass")
-fish_richness_merged_isl<- fish_richness_merged_isl %>% 
-  mutate(bycatch_biomass_bym3_mean_cat_isl = cut(bycatch_biomass_bym3_mean, xs_bycatch_biomass_bym3_mean, labels = labels_bycatch_biomass_bym3_mean))
-
-
-write.csv(fish_richness_merged_isl, "C:Biodiversity idea//Output files//master_island.csv", row.names=FALSE)
-
-head(fish_richness_merged_isl)
+length(unique(master_island$unq_isl))
+#101 
